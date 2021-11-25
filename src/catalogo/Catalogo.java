f439e1592cf1db8720aa302381824e3693d7e3ad
JonathanMartinez3 <95027155+JonathanMartinez3@users.noreply.github.com>
2021-11-25 08:45:20 -0500

    Update Catalogo.java

--- src/catalogo/Catalogo.java
+++ src/catalogo/Catalogo.java
@@ -1,4 +1,10 @@
 
+/*Faider Camilo Trujillo Olaya   20192020136
+Jonathan Sneyder Ardila Neira  20191007058
+Jonathan Estiben Martinez Malaver 20192020010 
+*/
+
+
 package catalogo;
 
 import java.util.Scanner;
